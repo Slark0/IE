4a985fc131d1bea48feec7142a019b76416f59ba
Slark0 <slark0@163.com>
2018-09-07 17:48:18 +0800

    2018.9.7

--- eventextractiontool.py
+++ eventextractiontool.py
@@ -1,7 +1,20 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import scrolledtext
-from tkinter import
+from tkinter import *
+
+# 导入tkinter模块的所有内容
+
+root = Tk()
+
+input = Entry(root)
+input.pack(padx=200,pady=200)
+
+input.delete(0, END)    #先清空按照索引
+input.insert(0,"请输入内容...")
+
+root.mainloop()
+
 
 
 
