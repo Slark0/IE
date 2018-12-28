dc49e5312db6e671c64901ccc6285ea3bf2b6ef3
Slark0 <slark0@163.com>
2018-12-28 18:09:24 +0800

    2018.12.28

--- test.py
+++ test.py
@@ -5,16 +5,15 @@ from tkinter.messagebox import showinfo, showwarning, showerror
 from functools import partial
 from tkinter import filedialog
 from tkinter import *
+import jieba
+import jieba.posseg as pseg
+seg_list = jieba.cut("我:来到北京清华大学“这里，有好多-朋友”", cut_all=False)
+print("Default Mode: " + "\ ".join(seg_list))
 
+words = pseg.cut("我:来到北京清华大学“这里，有好多-朋友”")
+for word, flag in words:
+    print("%s %s" % (word, flag))
 
-L = [1, 2]
-M = L
-L.append(3)
-L[0] = 9
-print(L, M)
-print("-------------------")
-L = [1, 2]
-M = L
-L += [3, 4]
-n = 5 % 2
-print(n)
+result = jieba.tokenize("净投放2000亿元农产品多数飘绿，审核门槛在提高，我:来到北京清华大学“这里，有好多-朋友")
+for tk in result:
+    print("word %s\t\t start:%d\t\t end:%d" % (tk[0],tk[1],tk[2]))
